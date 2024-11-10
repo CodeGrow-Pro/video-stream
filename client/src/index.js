59123724db279d16e616fedf4ef55805669088a1
CodeGrow-Pro <vk4993102@gmail.com>
2024-11-10 18:46:50 +0530

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="640666833805-3qp4n12r3aak3e5mmbpu56alm224htlu.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="846203985856-vm0lptja1is8fvm93jt82e525oqlr2fm.apps.googleusercontent.com">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
